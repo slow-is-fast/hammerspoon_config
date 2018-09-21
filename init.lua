111e1a4ff6b2b14fbebbc6ead77fe74e13461fbf
s4tar <89261073@qq.com>
2018-09-21 09:48:52 +0800

    move windows

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
-  hs.notify.new({title="Hammerspoon", informativeText="Hello World"}):send()
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "H", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+
+  f.x = f.x - 10
+  win:setFrame(f)
 end)
